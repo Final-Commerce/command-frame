8d761a80502c3639b725c6bc554e0ab69f681bdf
ploouf-oliver <hansram@oliverpos.com>
2026-06-04 11:15:37 -0230

    feat(FI-6493): add overlay command types [FI-6449]

---
+++ src/actions/resolve-extension-overlay/types.ts
@@ -0,0 +1,7 @@
+import type { InterceptorReturn } from "../../interceptors/types";
+export interface ResolveExtensionOverlayParams {
+    overlayId: string;
+    result: InterceptorReturn;
+}
+export interface ResolveExtensionOverlayResponse { success: boolean; }
+export type ResolveExtensionOverlay = (params: ResolveExtensionOverlayParams) => Promise<ResolveExtensionOverlayResponse>;
